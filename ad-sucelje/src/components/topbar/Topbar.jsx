9450b3fff7a425e22a39287d380002d3175e095c
ValentinV <valentin.vranjes@gmail.com>
2023-10-17 22:47:57 +0200

    dodane prve ikone i buttoni

--- ad-sucelje/src/components/topbar/Topbar.jsx
+++ ad-sucelje/src/components/topbar/Topbar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./topbar.css";
+import { NotificationsActiveOutlined, RecordVoiceOverOutlined, SettingsApplicationsOutlined } from "@mui/icons-material";
 
 const Topbar = () => {
   return (
-    <div>
-      Topbar
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Admin Dash</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsActiveOutlined />
+            <span className="topIconButton">7</span>
+          </div>
+          <div className="topbarIconContainer">
+            <RecordVoiceOverOutlined />
+            <span className="topIconButton">7</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsApplicationsOutlined />
+            <span className="topIconButton">7</span>
+          </div>
+        </div> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;

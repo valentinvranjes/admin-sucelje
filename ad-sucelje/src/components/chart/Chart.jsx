3bef7c063d4603a6f4f16a4048a9a99fc66826ec
ValentinV <valentin.vranjes@gmail.com>
2023-10-24 19:40:36 +0200

    dodan prvi dio grafa, XAxis

--- ad-sucelje/src/components/chart/Chart.jsx
+++ ad-sucelje/src/components/chart/Chart.jsx
@@ -1,12 +1,71 @@
 import React from 'react';
 import "./chart.css";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-const Chart = () => {
-  return (
-    <div className='chart'>
-      Chart
+const data = [
+    {
+        name: "Jan",
+        "Active User": 4000,
+      },
+      {
+        name: "Feb",
+        "Active User": 3000,
+      },
+      {
+        name: "Mar",
+        "Active User": 5000,
+      },
+      {
+        name: "Apr",
+        "Active User": 4000,
+      },
+      {
+        name: "May",
+        "Active User": 3000,
+      },
+      {
+        name: "Jun",
+        "Active User": 2000,
+      },
+      {
+        name: "Jul",
+        "Active User": 4000,
+      },
+      {
+        name: "Agu",
+        "Active User": 3000,
+      },
+      {
+        name: "Sep",
+        "Active User": 4000,
+      },
+      {
+        name: "Oct",
+        "Active User": 1000,
+      },
+      {
+        name: "Nov",
+        "Active User": 4000,
+      },
+      {
+        name: "Dec",
+        "Active User": 3000,
+      },
+];
+
+const Chart = ({ title, dataKey, grid }) => (
+    <div className="chart">
+      <h3 className="chartTitle">{title}</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
     </div>
-  )
-}
-
-export default Chart
+  );
+  
+  export default Chart;
